2302dd25cbc80497967e4e3bbb6e0ab2af30efa6
frosemarie92 <frosemarie92@gmail.com>
2022-05-11 21:57:53 -0700

    changed apikey

--- src/script.js
+++ src/script.js
@@ -74,7 +74,7 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiKey = "84bd2ad5ed0c3b8789486a62c63f2657";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
@@ -106,7 +106,7 @@ function displayTemperature(response) {
 }
 
 function search(city) {
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiKey = "84bd2ad5ed0c3b8789486a62c63f2657";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
